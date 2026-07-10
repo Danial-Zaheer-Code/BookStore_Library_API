aabd2da54f8174489c77f3edf49f3a77fdbc04af
Danial-Zaheer-Code <danialzaheer80@gmail.com>
2026-07-10 16:42:56 +0500

    Removed the rpository pattern and inlined all method used only once. Moved al other directly in services file.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
 import * as userServices from "../services/userServices.js"
-export async function register(req, res){
+export async function register(req, res) {
     const user = req.body
 
     const result = await userServices.register(user);
@@ -15,7 +15,7 @@ export async function login(req, res) {
     return res.status(result.status).json(result.responseBody)
 }
 
-export async function refresh(req, res){    
+export async function refresh(req, res) {
     const tokenPayload = {
         userId: req.userId,
         isAdmin: req.isAdmin
@@ -26,17 +26,18 @@ export async function refresh(req, res){
     return res.status(result.status).json(result.responseBody)
 }
 
-export async function retrieveProfile(req, res){
+export async function retrieveProfile(req, res) {
     const userId = req.userId
     const result = await userServices.retrieveProfile(userId)
-    
+
     return res.status(result.status).json(result.responseBody)
 }
 
-export async function updateUser(req, res){
-    const user = {}
-    user.id = req.userId
-    user.data = req.body ?? {}
+export async function updateUser(req, res) {
+    const user = {
+        id: req.userId,
+        data: req.body ?? {}
+    }
 
     const result = await userServices.updateUser(user)
 
@@ -44,15 +45,15 @@ export async function updateUser(req, res){
 
 }
 
-export async function listUsers(req, res){
+export async function listUsers(req, res) {
     const filters = req.body
     const result = await userServices.listUsers(filters)
 
     return res.status(result.status).json(result.responseBody)
-} 
+}
 
 export async function updateRole(req, res) {
-    const {userId, role} = req.body
+    const { userId, role } = req.body
 
     const user = {
         id: userId,
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,20 +1,23 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-import * as userRepository from "../repository/userRepository.js"
 import * as stausCode from "../utils/statusCodes.js"
+import { prisma } from "../lib/prisma.js"
 import { hash, compare } from "../utils/hashing.js"
 import { success, failure } from "../utils/result.js"
 import { createToken } from "../utils/utils.js"
 
 export async function register(user) {
     try {
-        if (await userRepository.isEmailTaken(user.email)) {
+        if (await isEmailTaken(user.email)) {
             return failure(stausCode.CONFLICT, "User already exists")
         }
 
         user.password = await hash(user.password);
-        await userRepository.createUser(user)
+        await prisma.user.create({
+            data: user
+        })
+
         return success(stausCode.OK, "User created successfully")
     } catch (error) {
         console.log(error)
@@ -24,7 +27,7 @@ export async function register(user) {
 
 export async function login(user) {
     try {
-        const existingUser = await userRepository.retrieveUser(user.email);
+        const existingUser = await retrieveUser(user.email);
         if (!existingUser) {
             return failure(stausCode.NOT_FOUND, "User does not exists")
         }
@@ -63,7 +66,7 @@ export async function refreshToken(tokenPayload) {
 
 export async function retrieveProfile(userId) {
     try {
-        const user = await userRepository.retrieveUserById(userId)
+        const user = await retrieveUserById(userId)
 
         if (!user) {
             return failure(stausCode.NOT_FOUND, "User does not exists")
@@ -79,11 +82,11 @@ export async function retrieveProfile(userId) {
 
 export async function updateUser(user) {
     try {
-        if (!await userRepository.isUserExists(user.userId)) {
+        if (!await isUserExists(user.id)) {
             return failure(stausCode.NOT_FOUND, "User does not exists")
         }
 
-        if (user.data.email && await userRepository.isEmailTaken(user.data.email)) {
+        if (user.data.email && await isEmailTaken(user.data.email)) {
             return failure(stausCode.CONFLICT, "Email already in use")
         }
 
@@ -91,7 +94,12 @@ export async function updateUser(user) {
             user.data.password = await hash(user.data.password)
         }
 
-        await userRepository.updateUser(user)
+        await prisma.user.update({
+            where: {
+                id: user.id
+            },
+            data: user.data
+        })
 
         return success(stausCode.OK, "Profile Updated successfully")
 
@@ -103,7 +111,32 @@ export async function updateUser(user) {
 
 export async function listUsers(filters) {
     try {
-        const users = await userRepository.listUsers(filters)
+        const users = await prisma.user.findMany({
+            take: filters.limit,
+            skip: (filters.page - 1) * filters.limit,
+            where: {
+                OR: [
+                    {
+                        name: {
+                            contains: filters.search ?? '',
+                            mode: "insensitive"
+                        }
+                    },
+                    {
+                        email: {
+                            contains: filters.search ?? '',
+                            mode: "insensitive"
+                        }
+                    }
+                ]
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                role: true
+            }
+        });
 
         return success(stausCode.OK, "Users retrieved successfully", { users: users })
     } catch (error) {
@@ -113,21 +146,64 @@ export async function listUsers(filters) {
 }
 
 export async function updateRole(user, adminId) {
-    console.log(user)
-    if (user.userId == adminId) {
+    if (user.id == adminId) {
         return failure(stausCode.FORBIDDEN, "You can't change your own role")
     }
 
-    if(!await userRepository.isUserExists(user.userId)){
+    if (!await isUserExists(user.id)) {
         return failure(stausCode.NOT_FOUND, "The user does not exists")
     }
 
     try {
-        await userRepository.updateUser(user)
+        await prisma.user.update({
+            where: {
+                id: user.id
+            },
+            data: user.data
+        })
 
         return success(stausCode.OK, "Role updated successfully")
     } catch (error) {
         console.log(error)
         return failure(stausCode.INTERNAL_SERVER_ERROR, "Something went wrong try again later")
     }
+}
+
+export async function isEmailTaken(email) {
+    const user = await retrieveUser(email)
+
+    return user != null;
+}
+
+export async function retrieveUser(email) {
+    return await prisma.user.findFirst({
+        where: {
+            email: email
+        },
+        select: {
+            id: true,
+            name: true,
+            password: true,
+            role: true
+        }
+    })
+}
+
+export async function isUserExists(id) {
+    const user = await retrieveUserById(id)
+    return user != null
+}
+
+export async function retrieveUserById(id) {
+    return await prisma.user.findUnique({
+        where: {
+            id: id
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            role: true
+        }
+    })
 }
