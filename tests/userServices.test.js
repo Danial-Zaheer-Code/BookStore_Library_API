b61e31b64ba296ac7f3310b5bc1b45daab7d6b1f
Danial-Zaheer-Code <danialzaheer80@gmail.com>
2026-07-10 14:41:01 +0500

    Added some more tests.

--- tests/userServices.test.js
+++ tests/userServices.test.js
@@ -1,4 +1,4 @@
-import { jest } from "@jest/globals";
+import { beforeEach, jest } from "@jest/globals";
 import { userRepositoryMock } from "./mocks/userRepositoryMock.js";
 
 jest.unstable_mockModule(
@@ -11,15 +11,42 @@ const userService = await import("../src/services/userServices.js");
 import { describe, test, expect } from "@jest/globals";
 import * as statusCodes from "../src/utils/statusCodes.js"
 describe("User Registration Tests", () => {
+    beforeEach(() => {
+        const user = {
+            email: "abc",
+            passsword: "123"
+        }
+    })
     test("Should return statusCode.CONFLICT if user already exists", async () => {
         userRepositoryMock.isEmailTaken.mockResolvedValue(true);
 
-        const result = await userService.register({
-            email: "abc"
-        })
+        const result = await userService.register(user)
 
         expect(result.status).toEqual(statusCodes.CONFLICT);
 
         expect(userRepositoryMock.isEmailTaken).toHaveBeenCalledWith("abc");
     });
+
+    test("Should return statusCode.INTERNAL_SERVER_ERROR if an exception is thrown", async () => {
+        userRepositoryMock.isEmailTaken.mockRejectedValue(
+            new Error("Database error")
+        );
+
+        const result = await userService.register({
+            email:"abc"
+        })
+
+        expect(result.status).toEqual(statusCodes.INTERNAL_SERVER_ERROR);
+    });
+
+    test("Should return statusCode.OK on successful user creation", async () => {
+        userRepositoryMock.isEmailTaken.mockResolvedValue(true);
+        userRepositoryMock.createUser.mockResolvedValue(true)
+
+        const result = await userService.register(user)
+
+        expect(result.status).toEqual(statusCodes.OK);
+
+        expect(userRepositoryMock.createUser).toHaveBeen(user);
+    })
 });
